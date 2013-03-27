1dcb5dd211e6b3091c92054b17ebc4cf984d868b
Daniel Ritchie <daniel.c.ritchie@gmail.com>
2013-03-26 19:36:31 -0700

    Proposal interface for ERPs, and deepcopy for random databases.

--- probabilistic/erp.py
+++ probabilistic/erp.py
@@ -24,6 +24,12 @@ class _RandomPrimitive:
 	def _logprob(self, val, params):
 		pass
 
+	def _proposal(self, val, params):
+		pass
+
+	def _logProposalProb(self, oldval, newval, params):
+		pass
+
 class _FlipRandomPrimitive(_RandomPrimitive):
 	"""
 	ERP with Bernoulli distribution
@@ -43,5 +49,12 @@ class _FlipRandomPrimitive(_RandomPrimitive):
 	def _logprob(self, val, params):
 		p = params[0]
 		return (p if val == 1 else 1.0-p)
+
+	def _proposal(self, val, params):
+		return int(not val)
+
+	def _logProposalProb(self, oldval, newval, params):
+		return 0.0 	# There's only one possible other value to proposal, so we have log(1.0)
+
 
 # TODO: Implement more ERP types!
